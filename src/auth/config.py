335f4ec02a132f29f3846fec179f61b3d3c9b9e3
mrkoc <mr.kocta@bk.ru>
2024-03-05 13:26:10 +0300

    (fix): change cookie transport

--- src/auth/config.py
+++ src/auth/config.py
@@ -10,5 +10,5 @@ def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(SECRET_AUTH, lifetime_seconds=3600)
 
 
-auth_backend = AuthenticationBackend(name="jwt", transport=bearer_transport, get_strategy=get_jwt_strategy)
+auth_backend = AuthenticationBackend(name="jwt", transport=cookie_transport, get_strategy=get_jwt_strategy)
 
